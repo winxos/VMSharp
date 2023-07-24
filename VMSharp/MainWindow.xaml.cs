25ce4467a95a510e4be4ec7371164da5b0236e7d
winxos <winxos@hotmail.com>
2023-07-24 17:06:52 +0800

    添加项目文件。

---
+++ VMSharp/MainWindow.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VMSharp
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void update_linenum()
+        {
+            var x = string.Empty;
+            for (var i = 0; i < input.LineCount && i < 2000; i++)
+            {
+                x += i.ToString("00\n");
+            }
+            linnum.Text = x;
+        }
+        Dictionary<string, int> tags = new Dictionary<string, int>();
+        Dictionary<string, int> keywords = new Dictionary<string, int>
+        {
+            {"READ",10 },{"WRITE",11},{"LOAD",20},{"STORE",21},
+            {"ADD",30 },{"SUB",31 },{"MUL",32 },{"DIV",33 },
+            {"JMP",40 },{"JMPN",41},{"JMPZ",42},
+            {"HALT",43 }
+        };
+        string pretranslate(string s)
+        {
+            string str = "";
+            string[] ls = s.Split('\n');
+            for (int i = 0; i < ls.Length; i++) //scan the :label
+            {
+                if (ls[i].StartsWith(":")) //label
+                {
+                    string ts = ls[i].Trim();
+                    string[] token = ts.Substring(1, ts.Length - 1).Split();
+                    if (token[0] != "")
+                    {
+                        tags[token[0]] = i;
+                        if (token.Length == 2) //:x 1, label with value
+                        {
+                            str += token[1] + "\n";
+                        }
+                        else
+                        {
+                            str += "0000\n";
+                        }
+                    }
+                    continue;
+                }
+                else if (ls[i] == "") str += "0000\n"; //blank
+                else str += ls[i] + "\n"; //normal
+            }
+            foreach (KeyValuePair<string, int> key in tags) //replace label
+            {
+                str = Regex.Replace(str, " " + key.Key + "\\W", string.Format(" {0:00}", key.Value));
+            }
+            return str;
+        }
+        string translate(string s) //to machine language
+        {
+            string sml = pretranslate(s);
+            foreach (var key in keywords)
+            {
+                sml = Regex.Replace(sml, key.Key + "\\W", string.Format("{0:00}", key.Value)); //op
+            }
+            sml = Regex.Replace(sml, "( )+", ""); //blank
+            sml = Regex.Replace(sml, @"\D43\D", "\n4300\n"); //halt
+            return sml;
+        }
+        private void input_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            update_linenum();
+            int pos = input.SelectionStart;
+            input.Text = input.Text.ToUpper();
+            input.Select(pos, 0);
+            if(mcode != null)
+            {
+                string s = translate(input.Text);
+                mcode.Text = s;
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            v.BindView(ref vmview);
+            update_linenum();
+            v.UpdateView();
+        }
+        VM v = new VM();
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string[] cs = mcode.Text.Trim().Split('\n');
+            var ccs = Array.ConvertAll(cs, int.Parse);
+            v.loadCode(ccs);
+            v.UpdateView();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            v.Step();
+            v.UpdateView();
+        }
+    }
+}
---
+++ VMSharp/VM.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace VMSharp
+{
+    internal class VM
+    {
+        Dictionary<int, string> keywords = new Dictionary<int, string>{
+            {10,"READ"},{11,"WRITE"},
+            {20,"LOAD"},{21,"STORE"},
+            {30,"ADD"},{31,"SUB"},{32,"MUL"},{33,"DIV"},
+            {40,"JMP"},{41,"JMPN"},{42,"JMPZ"},{43,"HALT"}};
+        const int MAX_MEM = 100;
+        const int Columns = 10;
+        int[] mem = new int[MAX_MEM];
+        int acc = 0;
+        int pc = 0;
+        int count = 0;
+        public Func<int> read = null;
+        public Action<int> write = null;
+        private TextBlock view;
+        public enum RUN_STATE
+        {
+            OK,
+            FINISHED,
+            OVERFLOW
+        }
+        RUN_STATE state;
+        public void loadCode(int[] cs)
+        {
+            Array.Clear(mem, 0, mem.Length);
+            cs.CopyTo(mem, 0);
+
+        }
+        public void BindView(ref TextBlock tb)
+        {
+            view = tb;
+        }
+        public void UpdateView()
+        {
+            view.Text  = $"{' ',10}Virtal Machine Sharp\n";
+            view.Text += $"{' ',30}winxos 2010-2023\n";
+            view.Text += $"     ACC:{acc,5}  PC:{pc,5} CNT:{count,5} STA:{state,5}\n";
+            view.Text += $"MEM:\n{' ',4}";
+            for (int i = 0; i < Columns; i++)
+            {
+                view.Inlines.Add(new Run($"{i,5}"));
+            }
+            view.Inlines.Add(new Run("\n"));
+            for (int i = 0; i < MAX_MEM / Columns; i++)
+            {
+                view.Inlines.Add(new Run($"{i * Columns,3}: "));
+                for (int j = 0; j < Columns; j++)
+                {
+                    if (i * 10 + j == pc)
+                    {
+                        view.Inlines.Add(new Run($"{mem[i * Columns + j]:0000} ")
+                        {
+                            Foreground = Brushes.Red
+                        });
+                    }
+                    else
+                    {
+                        view.Inlines.Add(new Run($"{mem[i * Columns + j]:0000} "));
+                    }
+                }
+                view.Inlines.Add(new Run("\n"));
+            }
+        }
+        public RUN_STATE Step()
+        {
+            if (pc >= mem.Length)
+            {
+                return RUN_STATE.OVERFLOW;
+            }
+            int code = mem[pc];
+            int opcode = code / mem.Length;
+            int operand = code % mem.Length;
+            count++;
+            if (keywords.ContainsKey(opcode))
+            {
+                switch (keywords[opcode])
+                {
+                    case "READ":
+                        if (read != null)
+                        {
+                            mem[operand] = read(); 
+                        }
+                        break;
+                    case "WRITE":
+                        if (write != null)
+                        {
+                            write(mem[operand]);
+                        }
+                        break;
+                    case "LOAD": acc = mem[operand]; break;
+                    case "STORE": mem[operand] = acc; break;
+                    case "ADD": acc += mem[operand]; break;
+                    case "SUB": acc -= mem[operand]; break;
+                    case "MUL": acc *= mem[operand]; break;
+                    case "DIV": acc /= mem[operand]; break;
+                    case "JMP": pc = operand - 1; break;
+                    case "JMPN": pc = acc < 0 ? operand - 1 : pc; break;
+                    case "JMPZ": pc = acc == 0 ? operand - 1 : pc; break;
+                    case "HALT":  return RUN_STATE.FINISHED;
+                }
+            }
+            pc++;
+            return RUN_STATE.OK;
+        }
+    }
+}
